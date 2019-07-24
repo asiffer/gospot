a7a1889dcf4c7ddb1748d50334d843e40b4cc6b2
Alban Siffer <alban.siffer@irisa.fr>
2019-07-24 13:14:35 +0200

    add json tags for marshalling

--- config.go
+++ config.go
@@ -9,21 +9,21 @@ import (
 // SpotConfig is the structure embedding the Spot configuration
 type SpotConfig struct {
 	// the main parameter ( P(X>zQ) < q )
-	Q float64
+	Q float64 `json:"q"`
 	// number of observation to perform calibration
-	Ninit int
+	Ninit int `json:"n_init"`
 	// level of the update threshold (0<l<1)
-	Level float64
+	Level float64 `json:"level"`
 	// if true, compute upper threshold
-	Up bool
+	Up bool `json:"up"`
 	// if true, compute lower threshold
-	Down bool
+	Down bool `json:"down"`
 	// if true, the algorithm triggers alarms (the outlier is not taking into account in the model)
-	Alert bool
+	Alert bool `json:"alert"`
 	// if true, the number of stored will be bounded by max_excess
-	Bounded bool
+	Bounded bool `json:"bounded"`
 	// Maximum number of stored excesses (bounded mode)
-	MaxExcess int
+	MaxExcess int `json:"max_excess"`
 }
 
 // DSpotConfig is the structure embedding the DSpot config (SpotConfig + depth)
@@ -32,50 +32,6 @@ type DSpotConfig struct {
 	// Depth is the size of the underlying moving average
 	Depth int
 }
-
-// var (
-// 	spotConfigNew      func(uintptr) uintptr
-// 	spotConfigDelete   func(uintptr)
-// 	configGetQ         func(uintptr) float64
-// 	configGetBounded   func(uintptr) int32
-// 	configGetMaxExcess func(uintptr) int32
-// 	configGetAlert     func(uintptr) int32
-// 	configGetUp        func(uintptr) int32
-// 	configGetDown      func(uintptr) int32
-// 	configGetNinit     func(uintptr) int32
-// 	configGetLevel     func(uintptr) float64
-// 	// DefaultSpotConfig is the default structure to create a Spot Object
-// 	DefaultSpotConfig SpotConfig
-// 	// DefaultDSpotConfig is the default structure to create a DSpot Object
-// 	DefaultDSpotConfig DSpotConfig
-// )
-
-// var configSymbols = map[string]interface{}{
-// 	"Spot_config_ptr":        &spotConfigNew,
-// 	"Spot_config_delete":     &spotConfigDelete,
-// 	"_config_get_q":          &configGetQ,
-// 	"_config_get_bounded":    &configGetBounded,
-// 	"_config_get_max_excess": &configGetMaxExcess,
-// 	"_config_get_alert":      &configGetAlert,
-// 	"_config_get_up":         &configGetUp,
-// 	"_config_get_down":       &configGetDown,
-// 	"_config_get_n_init":     &configGetNinit,
-// 	"_config_get_level":      &configGetLevel,
-// }
-
-// // LoadSymbolsConfig loads the symbols related to the SpotStatus object
-// // from the C++ library libspot. It returns an error if a loading fails, nil
-// // pointer otherwise
-// func LoadSymbolsConfig() error {
-// 	var err error
-// 	for k, f := range configSymbols {
-// 		err = libspot.Sym(k, f)
-// 		if err != nil {
-// 			return fmt.Errorf("Error in loading %s (%s)", k, err.Error())
-// 		}
-// 	}
-// 	return nil
-// }
 
 func (sc SpotConfig) String() string {
 	return fmt.Sprintf("%10s %.6f\n%10s %d\n%10s %.6f\n%10s %t\n%10s %t\n%10s %t\n%10s %t\n%10s %d\n",
--- status.go
+++ status.go
@@ -10,27 +10,27 @@ import (
 // SpotStatus is the structure embedding the status of a Spot instance
 type SpotStatus struct {
 	// N is the number of normal observations (not the alarms)
-	N int
-	// ExUp is the urrent number of up excesses
-	ExUp int
+	N int `json:"n"`
+	// ExUp is the current number of up excesses
+	ExUp int `json:"ex_up"`
 	// ExDown is the current number of down excesses
-	ExDown int
+	ExDown int `json:"ex_down"`
 	// NtUp is the total number of up excesses
-	NtUp int
+	NtUp int `json:"Nt_up"`
 	// NtDown is the total number of down excesses
-	NtDown int
+	NtDown int `json:"Nt_down"`
 	// AlUp is the number of up alarms
-	AlUp int
+	AlUp int `json:"al_up"`
 	// AlDown is the number of down alarms
-	AlDown int
+	AlDown int `json:"al_down"`
 	// TUp is the transitional up threshold
-	TUp float64
+	TUp float64 `json:"t_up"`
 	// TDown is the transitional down threshold
-	TDown float64
+	TDown float64 `json:"t_down"`
 	// ZUp is the up alert thresholds
-	ZUp float64
+	ZUp float64 `json:"th_up"`
 	// ZDown is the down alert thresholds
-	ZDown float64
+	ZDown float64 `json:"th_down"`
 }
 
 // NewSpotStatus creates a new empty spot status structure
@@ -56,52 +56,6 @@ type DSpotStatus struct {
 	// Mean is the the value of the current local model
 	Mean float64
 }
-
-// var (
-// 	spotStatusNew    func(uintptr) uintptr
-// 	spotStatusDelete func(uintptr)
-// 	statusGetN       func(uintptr) int32
-// 	statusGetExUp    func(uintptr) int32
-// 	statusGetExDown  func(uintptr) int32
-// 	statusGetNtUp    func(uintptr) int32
-// 	statusGetNtDown  func(uintptr) int32
-// 	statusGetAlUp    func(uintptr) int32
-// 	statusGetAlDown  func(uintptr) int32
-// 	statusGetTUp     func(uintptr) float64
-// 	statusGetTDown   func(uintptr) float64
-// 	statusGetZUp     func(uintptr) float64
-// 	statusGetZDown   func(uintptr) float64
-// )
-
-// var statusSymbols = map[string]interface{}{
-// 	"Spot_status_ptr":     &spotStatusNew,
-// 	"Spot_status_delete":  &spotStatusDelete,
-// 	"_status_get_n":       &statusGetN,
-// 	"_status_get_ex_up":   &statusGetExUp,
-// 	"_status_get_ex_down": &statusGetExDown,
-// 	"_status_get_Nt_up":   &statusGetNtUp,
-// 	"_status_get_Nt_down": &statusGetNtDown,
-// 	"_status_get_al_up":   &statusGetAlUp,
-// 	"_status_get_al_down": &statusGetAlDown,
-// 	"_status_get_t_up":    &statusGetTUp,
-// 	"_status_get_t_down":  &statusGetTDown,
-// 	"_status_get_z_up":    &statusGetZUp,
-// 	"_status_get_z_down":  &statusGetZDown,
-// }
-
-// LoadSymbolsStatus It loads the symbols related to the SpotStatus object
-// from the C++ library libspot. It returns an error if a loading fails, nil
-// pointer otherwise
-// func LoadSymbolsStatus() error {
-// 	var err error
-// 	for k, f := range statusSymbols {
-// 		err = libspot.Sym(k, f)
-// 		if err != nil {
-// 			return fmt.Errorf("Error in loading %s (%s)", k, err.Error())
-// 		}
-// 	}
-// 	return nil
-// }
 
 func (ss SpotStatus) String() string {
 	return fmt.Sprintf("%8s %d\n%8s %d\n%8s %d\n%8s %d\n%8s %d\n%8s %d\n%8s %d\n%8s %.6f\n%8s %.6f\n%8s %.6f\n%8s %.6f\n",
